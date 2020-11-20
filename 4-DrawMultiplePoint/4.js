79972a3b2ba71cb5f23d2a2cd94603af2b26c662
swpuhu <swpujita@126.com>
2020-11-20 09:29:10 +0800

    add example

---
+++ 4-DrawMultiplePoint/4.js
@@ -0,0 +1,78 @@
+import { initWebGL } from "../util.js";
+
+/**
+ * @type {HTMLCanvasElement}
+ */
+const canvas = document.getElementById("canvas");
+const gl = canvas.getContext("webgl");
+
+const vertexShader = `
+attribute vec4 a_position;
+attribute vec4 a_color;
+varying vec4 v_color;
+void main () {
+    gl_Position = a_position;
+    v_color = a_color;
+}  
+`;
+
+const fragmentShader = `
+    precision mediump float;
+    varying vec4 v_color;
+    void main () {
+        gl_FragColor = v_color;
+    }
+`;
+const pointPos = [
+    -0.5,
+    0.0,
+    1.0,
+    0.0,
+    0.0,
+    1.0,
+    0.5,
+    0.0,
+    0.0,
+    1.0,
+    0.0,
+    1.0,
+    0.0,
+    0.5,
+    0.0,
+    0.0,
+    1.0,
+    1.0,
+];
+
+console.log(pointPos.length);
+
+const program = initWebGL(gl, vertexShader, fragmentShader);
+gl.useProgram(program);
+
+const buffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(pointPos), gl.STATIC_DRAW);
+
+const a_position = gl.getAttribLocation(program, "a_position");
+const a_color = gl.getAttribLocation(program, "a_color");
+gl.vertexAttribPointer(
+    a_position,
+    2,
+    gl.FLOAT,
+    false,
+    Float32Array.BYTES_PER_ELEMENT * 6,
+    0
+);
+
+gl.vertexAttribPointer(
+    a_color,
+    4,
+    gl.FLOAT,
+    false,
+    Float32Array.BYTES_PER_ELEMENT * 6,
+    Float32Array.BYTES_PER_ELEMENT * 2
+);
+gl.enableVertexAttribArray(a_position);
+gl.enableVertexAttribArray(a_color);
+
+gl.drawArrays(gl.TRIANGLES, 0, 3);
